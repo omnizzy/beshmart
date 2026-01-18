9cb4c1d8c04267520ced6c47a633d97074197231
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-01-18 16:58:43 +0100

    product service

--- product-service/src/test/java/com/besh/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/besh/microservices/product/ProductServiceApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
